64154c2a50dc305da7dee448f2e625ac036820c7
Karthik A <akarthik101@gmail.com>
2017-11-24 02:35:45 -0500

    Remove unused imports

--- src/main/java/edu/umass/cs/mongoquerygen/main/Main.java
+++ src/main/java/edu/umass/cs/mongoquerygen/main/Main.java
@@ -1,9 +1,5 @@
 package edu.umass.cs.mongoquerygen.main;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import edu.umass.cs.mongoquerygen.parser.ParseExpression;
 
 /**
@@ -12,16 +8,6 @@ import edu.umass.cs.mongoquerygen.parser.ParseExpression;
 public class Main
 {
     public static void main(String[] args) {
-        JsonElement jsonElement = new JsonParser().parse("{'a':'b', 'c':1}");
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-        JsonArray jsonArray = new JsonArray();
-        jsonArray.add("A");
-        jsonArray.add("B");
-        jsonArray.add(1);
-
-        jsonObject.add("arr", jsonArray);
-        System.out.println(jsonObject.toString());
 
 
         String expr = "and(\n" +
